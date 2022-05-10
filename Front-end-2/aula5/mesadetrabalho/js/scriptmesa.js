1636bea5d538a7bbb300f88cd3b01cb41e802993
Victor Luz <85527775+vitinop@users.noreply.github.com>
2022-05-10 20:02:16 -0300

    update

--- Front-end-2/aula5/mesadetrabalho/js/scriptmesa.js
+++ Front-end-2/aula5/mesadetrabalho/js/scriptmesa.js
@@ -1,14 +1,19 @@
 
 const togglebox = document.querySelector('#toggle-theme-box')
+const items = document.querySelectorAll('.item')
 
 togglebox.addEventListener('change', function(){
     const bg_body = document.body
+    const bg_nav = document.querySelector('.nav')
+    bg_nav.classList.toggle('dark-nav')
     bg_body.classList.toggle('dark-bg')
+    
+    items.forEach(function(option) {
+
+      option.classList.toggle('dark-item')
+
+  }
+  ) 
 })
 
-const titulo = document.querySelector('#pageTitle')
-
-titulo.addEventListener('change', function(){
-    const titulo_change = document.getElementsByID("pageTitle")
-    titulo_change.classList.toggle('dark-h1')
-})
+    
