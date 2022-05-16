e6d304e900b71b6e718ab1871fe9e893f0af8bc0
Victor Luz <85527775+vitinop@users.noreply.github.com>
2022-05-16 15:58:07 -0300

    update

--- Front-end-2/aula7/script.js
+++ Front-end-2/aula7/script.js
@@ -1,6 +1,7 @@
 const imagens = document.querySelectorAll(".cartao img");
 const urlImagens = [];
 
+
 imagens.forEach(function (_, contador){
     const url = prompt(`Adicione o url para a imagem ${contador + 1}`);
     urlImagens.push(url);
@@ -11,6 +12,16 @@ imagens.forEach(function (_, contador){
 
 urlImagens.forEach(function(url, index) {
     const imagem = document.querySelector(`#imagem-${index +1}`);
-    imagem.setAttribute("src", url)
+    imagem.setAttribute("src", url). addEventListener("click", function() {
+        window.open(url, "_blank");
+    })
 
+
+   
+    
 });
+
+
+
+
+
